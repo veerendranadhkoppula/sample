c33a82b2ba1ed400bdc2e8eb5f3878fad8d1309e
veerendranadhkoppula <veerendrakoppula68@gmail.com>
2025-06-10 15:29:18 +0530

    hello

--- app/page.js
+++ app/page.js
@@ -15,9 +15,9 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.js</code>.
+           veerendra <code>app/page.js</code>.
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>sample</li>
         </ol>
 
         <div className={styles.ctas}>
